5b4da209a0fdf5f671f10849e0a39be7541434b2
mwsohn <mwsohn@gmail.com>
2025-11-03 00:47:34 -0500

    Create FreqTools.jl

---
+++ src/FreqTools.jl
@@ -0,0 +1,27 @@
+module TableTools
+
+################################################################################
+##
+## Dependencies
+##
+################################################################################
+
+using DataFrames, Stella, FreqTables, NamedArrays, FreqTables,
+    Printf, CategoricalArrays, PrettyTables, Reexport, Statistics
+
+##############################################################################
+##
+## Exported methods and types (in addition to everything reexported above)
+##
+##############################################################################
+
+export tab, rowpct, colpct, cellpct
+
+##############################################################################
+##
+## Load files
+##
+##############################################################################
+include("tab.jl")
+
+end
